2d0d28fe6094c97e987acc66e28195ab46c873b9
TetianaSoloviova <soltvmail@gmail.com>
2025-09-01 22:19:29 +0300

    Додані тести http та додано .gitignore для виключення кеш-файлів

---
+++ tests/api/test_http.py
@@ -0,0 +1,25 @@
+import pytest
+import requests
+
+@pytest.mark.http
+def test_first_request():
+    r = requests.get('https://api.github.com/zen')
+    print(f"Response is {r.text}")
+
+
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('https://api.github.com/users/defunkt')
+    body = r.json()
+    headers = r.headers
+
+    assert body['name'] == 'Chris Wanstrath'
+    assert r.status_code == 200
+    assert headers['Server'].lower() == 'github.com'    
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get('https://api.github.com/users/tetiana_soloviova')
+
+    assert r.status_code == 404
